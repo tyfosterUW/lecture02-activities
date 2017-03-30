0fbb02805241542db038b43a4f5994790f7d7459
iGuest <tyfoster@uw.edu>
2017-03-30 15:23:39 -0700

    Lecture finished

--- android/ActivityDemo/app/src/main/java/edu/uw/activitydemo/MainActivity.java
+++ android/ActivityDemo/app/src/main/java/edu/uw/activitydemo/MainActivity.java
@@ -1,13 +1,61 @@
 package edu.uw.activitydemo;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity"; //Generally named after activity or app
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "You clicked me :D!!");
+            }
+        });
+        Log.v(TAG, "onCreate called");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //do stuff
+        Log.v(TAG, "onStart called");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.v(TAG, "onStop called");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        System.out.println("onresume");
+        Log.v(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.v(TAG, "onDestroy called");
+
     }
 }
--- java/src/main/java/MyGUI.java
+++ java/src/main/java/MyGUI.java
@@ -5,25 +5,50 @@ import java.util.*;
 /**
  * A demo of GUI programming with Swing
  */
-public class MyGUI extends JFrame {
-	
-	public MyGUI() {
-		super("The Skywalker Family");
+public class MyGUI extends JFrame{
+
+	public MyGUI(){
+		super("GUI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton theButton = new JButton("Click me!");
 
+		theButton.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("clicky");
+			}
+		} );
+
+		JButton theButton2 = new JButton("Click me 2: Electric Clickaroo!");
+
+		theButton2.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("haha I won");
+			}
+		} );
+
+		JPanel container = new JPanel();
+		container.setLayout(new BoxLayout(container,  BoxLayout.Y_AXIS));
+
+		container.add(theButton);
+		container.add(theButton2);
+
+		JPanel otherContainer = new JPanel();
+		otherContainer.setLayout(new BoxLayout(otherContainer, BoxLayout.X_AXIS));
+		otherContainer.add(new JButton("A"));
+		otherContainer.add(new JButton("B"));
+		otherContainer.add(new JButton("C"));
 
 
 
-		
-		this.add(theButton);
-		
+		this.add(container);
+		container.add(otherContainer);
+
 		this.pack();
 		this.setVisible(true);		
 	}
 	
-	public static void main(String[] args) {
+  public static void main(String[] args) {
     new MyGUI(); //create the frame
   }
 }
